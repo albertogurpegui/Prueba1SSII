b2a3738910ecc76609766955e1c5b79414c3f8e1
albertogurpegui <alberto.gurpeguiramon@gmail.com>
2018-03-15 12:06:59 +0100

    He modificado

--- Prueba1/ViewController.swift
+++ Prueba1/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var image1: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
