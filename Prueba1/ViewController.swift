28fd36069a1c685a3976f5e7a44fc83d760dcaa1
ALBERTO GURPEGUI RAMÓN <alberto.gurpegui@utmac0013.local>
2018-03-15 10:01:31 +0100

    Initial Commit

---
+++ Prueba1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Prueba1
+//
+//  Created by ALBERTO GURPEGUI RAMÓN on 15/3/18.
+//  Copyright © 2018 ALBERTO GURPEGUI RAMÓN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
